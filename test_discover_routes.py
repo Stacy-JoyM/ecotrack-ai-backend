56ba2aa6f99008ee76e4ede867f40ab4d143c6f7
sarahsululu <sarahsululu47@gmail.com>
2025-10-22 15:08:28 +0300

    Update test_discover_routes.py

--- test_discover_routes.py
+++ test_discover_routes.py
@@ -2,7 +2,7 @@ from flask import Flask
 from routes.discover_routes import discover_bp
 
 app = Flask(__name__)
-app.config['MAPBOX_API_KEY'] = 'your_mapbox_api_key_here'  # <-- put your real key here
+app.config['MAPBOX_API_KEY'] = 'your_mapbox_api_key_here'  
 app.register_blueprint(discover_bp)
 
 if __name__ == "__main__":
